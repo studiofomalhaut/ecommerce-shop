8ce6f785c4b784fed33a3564ab5aada262601530
Studio Fomalhaut <studio@fomalhaut.nl>
2022-07-10 00:50:10 +0200

    updating ecommerce shop with categories component and categories item component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import useEffect from 'react'
+import CategoryContainer from './components/category-container/category-container.component'
+import './App.styles.scss'
+
+const App = () => {
+
+  return (
+    <CategoryContainer />
+  );
+}
+
+export default App;
